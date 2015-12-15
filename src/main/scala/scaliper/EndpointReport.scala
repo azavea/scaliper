7bff23bb2b8fb33db524f835868524b3a68e1256
lossyrob <rdemanuele@gmail.com>
2015-12-14 22:49:48 -0500

    Allow skipping of failures for EndpointReport

--- src/main/scala/scaliper/EndpointReport.scala
+++ src/main/scala/scaliper/EndpointReport.scala
@@ -11,25 +11,28 @@ import com.typesafe.config.ConfigFactory
 
 /** Sends a JSON representation of the run to an endpoint */
 trait EndpointReport extends Benchmarks {
-  private val (applicationName, endpoint) = {
+  private val (applicationName, endpoint, failIfError) = {
     val conf = ConfigFactory.load()
     (conf.getString("scaliper.application.name"),
-      conf.getString("scaliper.report.endpoint"))
+      conf.getString("scaliper.report.endpoint.url"),
+      conf.getBoolean("scaliper.report.endpoint.failIfError"))
   }
 
   abstract override def report(run: Run): Unit = {
-    EndpointReport.report(NamedRun(applicationName, run), endpoint)
+    EndpointReport.report(NamedRun(applicationName, run), endpoint, failIfError)
     super.report(run)
   }
 }
 
 object EndpointReport {
-  def report(r: NamedRun, endpoint: String): Unit = {
+  def report(r: NamedRun, endpoint: String, failIfError: Boolean): Unit = {
     val svc = url(endpoint).setContentType("application/json", "UTF-8") << r.toJson.compactPrint
     try {
       Await.result(Http(svc OK as.String), 1 second)
     } catch {
-      case e: Exception => throw new Exception(s"Failed to send run information to $endpoint").initCause(e)
+      case e: Exception =>
+        if(failIfError) throw new Exception(s"Failed to send run information to $endpoint").initCause(e)
+        else { println(s"Reporting to endpoint $endpoint failed: $e") }
     }
   }
 }
