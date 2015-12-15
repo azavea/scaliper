ee3c715cea4758267f2e336c01d8fb45de05f5c2
lossyrob <rdemanuele@gmail.com>
2015-12-15 00:26:47 -0500

    Added travis config

--- project/Version.scala
+++ project/Version.scala
@@ -1,5 +1,7 @@
+import scala.util.Properties
+
 object Version {
-  val scaliper = "0.5.0"
+  val scaliper = "0.5.0" + Properties.envOrElse("SCALIPER_VERSION_SUFFIX", "-SNAPSHOT")
   val scala = "2.10.6"
   val sprayJson = "1.3.2"
   val spray = "1.3.3"
